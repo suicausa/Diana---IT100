ebd8b85d567bcbc04e4a4ad7b08cb20c3f9fc201
suicausa <diana.person@gmail.com>
2013-09-27 11:51:38 -0400

    Update CollectNewspaperKarel.java

--- Assignment1/CollectNewspaperKarel.java
+++ Assignment1/CollectNewspaperKarel.java
@@ -13,7 +13,7 @@ import stanford.karel.*;
 public class CollectNewspaperKarel extends SuperKarel {
 	public void run() {
 		
-		//going to beeper
+		//go to beeper
 		while(frontIsClear()) {
 			move();
 		}
